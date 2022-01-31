ca3ac61fa0b68d16265f618e44719497f63a46f1
nicholasisaacs4 <nicholasisaacs4@gmail.com>
2022-01-30 22:22:42 -0500

    withdraw tests

--- src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
@@ -19,9 +19,15 @@ class BankAccountTest {
     void withdrawTest() throws InsufficientFundsException{
         BankAccount bankAccount = new BankAccount("a@b.com", 200);
         bankAccount.withdraw(100);
-
         assertEquals(100, bankAccount.getBalance(), 0.001);
         assertThrows(InsufficientFundsException.class, () -> bankAccount.withdraw(300));
+        BankAccount bankAccount01 = new BankAccount("a@b.com", 500);
+        assertThrows(InsufficientFundsException.class, () -> bankAccount01.withdraw(600));
+        BankAccount bankAccount02 = new BankAccount("a@b.com", 600); 
+        bankAccount02.withdraw(300);
+        assertEquals(300, bankAccount02.getBalance(), 0.001);
+        bankAccount02.withdraw(300);
+        assertEquals(0, bankAccount02.getBalance(), 0.001);
     }
 
     @Test
